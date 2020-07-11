0af786c27f044f57def292cf3ff497e657db9b92
satishs97 <60621934+satishs97@users.noreply.github.com>
2020-07-11 20:09:59 +0530

    Update Firebase.js

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyC0PtmPwxOYaAKnGpk9W6NbqmBNGkskpUg",
-    authDomain: "todos-auth-f0900.firebaseapp.com",
-    databaseURL: "https://todos-auth-f0900.firebaseio.com",
-    projectId: "todos-auth-f0900",
-    storageBucket: "todos-auth-f0900.appspot.com",
-    messagingSenderId: "831631056184",
-    appId: "1:831631056184:web:8cecf7a37651436a1035cd",
-    measurementId: "G-64QWLVKM7K"
+    apiKey: 
+    authDomain: 
+    databaseURL: 
+    projectId: 
+    storageBucket: 
+    messagingSenderId: 
+    appId: 
+    measurementId: 
 };
 
 firebase.initializeApp(config);
